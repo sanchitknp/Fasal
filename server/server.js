a4c764fbc4d3c8e8d871f43ab6c75a0c9b0ccfb3
sanchit <sanchitagarwal.mec18@itbhu.ac.in>
2021-12-17 13:18:06 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -15,5 +15,5 @@ console.log("incoming")
 app.use("/auth", userRoute);
 app.use("/playlist", playlistRoute);
 
-
-app.listen(5000, console.log(`Server running on  port 5000`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running on  port 5000`));
